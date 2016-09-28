e2cd958839dc6ed0b1a33f85707ad3cdd8d3674b
Andres Medina <amedina@pivotal.io>
2016-09-28 16:06:35 -0700

    Add debug flag
    Signed-off-by: Alpha Chen <achen@pivotal.io>

--- src/stackdriver-nozzle/main.go
+++ src/stackdriver-nozzle/main.go
@@ -4,22 +4,36 @@ import (
 	"github.com/evandbrown/gcp-tools-boshrelease/src/stackdriver-nozzle/firehose"
 	"github.com/evandbrown/gcp-tools-boshrelease/src/stackdriver-nozzle/stackdriver"
 
-	"github.com/evandbrown/gcp-tools-boshrelease/src/stackdriver-nozzle/nozzle"
 	"github.com/evandbrown/gcp-tools-boshrelease/src/stackdriver-nozzle/dev"
+	"github.com/evandbrown/gcp-tools-boshrelease/src/stackdriver-nozzle/nozzle"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	apiEndpoint = kingpin.Flag("api-endpoint", "Api endpoint address. For bosh-lite installation of CF: https://api.10.244.0.34.xip.io").
-			OverrideDefaultFromEnvar("API_ENDPOINT").Required().String()
-	user = kingpin.Flag("user", "Admin user.").Default("admin").
-		OverrideDefaultFromEnvar("FIREHOSE_USER").String()
-	password = kingpin.Flag("password", "Admin password.").Default("admin").
-			OverrideDefaultFromEnvar("FIREHOSE_PASSWORD").String()
-	skipSSLValidation = kingpin.Flag("skip-ssl-validation", "Please don't").Default("false").
-				OverrideDefaultFromEnvar("SKIP_SSL_VALIDATION").Bool()
+	debug = kingpin.Flag("debug", "send events to stdout").
+		OverrideDefaultFromEnvar("DEBUG").
+		Default(false).
+		Bool()
+	apiEndpoint = kingpin.Flag("api-endpoint",
+		"CF API endpoint (use https://api.bosh-lite.com for BOSH Lite)").
+		OverrideDefaultFromEnvar("API_ENDPOINT").
+		Required().
+		String()
+	user = kingpin.Flag("user", "username").
+		Default("admin").
+		OverrideDefaultFromEnvar("FIREHOSE_USER").
+		String()
+	password = kingpin.Flag("password", "password").
+			Default("admin").
+			OverrideDefaultFromEnvar("FIREHOSE_PASSWORD").
+			String()
+	skipSSLValidation = kingpin.Flag("skip-ssl-validation", "please don't").
+				Default("false").
+				OverrideDefaultFromEnvar("SKIP_SSL_VALIDATION").
+				Bool()
 	projectID = kingpin.Flag("project-id", "gcp project id").
-			OverrideDefaultFromEnvar("PROJECT_ID").String() //maybe we can get this from gcp env...? research
+			OverrideDefaultFromEnvar("PROJECT_ID").
+			String() //maybe we can get this from gcp env...? research
 )
 
 func main() {
@@ -35,8 +49,7 @@ func main() {
 
 	client := firehose.NewClient(config)
 
-	debug := false
-	if debug {
+	if *debug {
 		println("Sending firehose to standard out")
 		client.StartListening(&dev.StdOut{})
 	} else {
@@ -46,8 +59,5 @@ func main() {
 		n := nozzle.Nozzle{StackdriverClient: sdClient}
 
 		client.StartListening(&n)
-
-
 	}
-
 }
