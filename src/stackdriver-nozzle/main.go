ebb87d1926bc3c259857abd34b5b6462796221a4
Andres Medina <amedina@pivotal.io>
2016-09-28 11:31:58 -0700

    Add config via commmand line args or environment vars
    Signed-off-by: Alpha Chen <achen@pivotal.io>

--- src/stackdriver-nozzle/main.go
+++ src/stackdriver-nozzle/main.go
@@ -4,17 +4,34 @@ import (
 	"fmt"
 	"github.com/evandbrown/gcp-tools-boshrelease/src/stackdriver-nozzle/firehose"
 	"github.com/evandbrown/gcp-tools-boshrelease/src/stackdriver-nozzle/stackdriver"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	apiEndpoint       = kingpin.Flag("api-endpoint", "Api endpoint address. For bosh-lite installation of CF: https://api.10.244.0.34.xip.io").
+		OverrideDefaultFromEnvar("API_ENDPOINT").Required().String()
+	user              = kingpin.Flag("user", "Admin user.").Default("admin").
+		OverrideDefaultFromEnvar("FIREHOSE_USER").String()
+	password          = kingpin.Flag("password", "Admin password.").Default("admin").
+		OverrideDefaultFromEnvar("FIREHOSE_PASSWORD").String()
+	skipSSLValidation = kingpin.Flag("skip-ssl-validation", "Please don't").Default("false").
+		OverrideDefaultFromEnvar("SKIP_SSL_VALIDATION").Bool()
+	projectID = kingpin.Flag("project-id", "gcp project id").
+		OverrideDefaultFromEnvar("PROJECT_ID").String() //maybe we can get this from gcp env...? research
 )
 
 func main() {
-	sdClient := stackdriver.NewClient("")
-	sdClient.Post("hello world 4")
+	kingpin.Parse()
+
+	sdClient := stackdriver.NewClient(*projectID)
+	sdClient.Post("hello world 5")
 
 	config := &firehose.ClientConfig{
-		User: "admin",
-		Password: "",
-		ApiEndpoint: "",
-		SkipSSLValidation: true,
+		User:                 *user,
+		Password:             *password,
+		ApiEndpoint:          *apiEndpoint,
+		SkipSSLValidation:    *skipSSLValidation,
 	}
 
 	client := firehose.NewClient(config)
