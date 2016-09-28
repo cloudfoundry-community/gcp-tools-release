5629c0f59a090ddb2882b3dfbb6eabe6bc81c86a
Alpha Chen <achen@pivotal.io>
2016-09-27 17:04:29 -0700

    Implement actual stackdriver adapter
    Signed-off-by: Andres Medina <amedina@pivotal.io>

--- src/stackdriver-nozzle/main.go
+++ src/stackdriver-nozzle/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 )
+import "github.com/evandbrown/gcp-tools-boshrelease/src/stackdriver-nozzle/sink"
 
 func main() {
-
+	client := sink.NewStackdriverClient()
+	client.Post("Hello world again")
 }
--- src/stackdriver-nozzle/sink/stackdriver_client.go
+++ src/stackdriver-nozzle/sink/stackdriver_client.go
@@ -1,17 +1,41 @@
 package sink
 
+import (
+	"cloud.google.com/go/logging"
+	"golang.org/x/net/context"
+)
+
 type StackdriverClient interface {
 	Post(payload interface{})
 }
 
+type stackdriverClient struct {
+	logger *logging.Logger
+}
 
-/*
+// TODO Auth? We're currently relying on auto-auth
+// TODO error handling
+func NewStackdriverClient() StackdriverClient {
+	ctx := context.Background()
 
+	client, err := logging.NewClient(ctx, "evandbrown17")
+	if err != nil {
+		panic(err)
+	}
 
-3	type MockSplunkClient struct {
-4		CapturedEvents []map[string]interface{}
-5		PostBatchFn    func(events []map[string]interface{}) error
-6	}
-7
-8	func (m *MockSplunkClient) Post(events []map[string]interface{}) error {
- */
+	client.OnError = func(err error) {
+		panic(err)
+	}
+
+	logger := client.Logger("logs/cf_logs")
+
+	return &stackdriverClient{logger: logger}
+}
+
+func (s *stackdriverClient) Post(payload interface{}) {
+	entry := logging.Entry{
+		Payload: payload,
+	}
+	s.logger.Log(entry)
+	s.logger.Flush() // TODO ???
+}
