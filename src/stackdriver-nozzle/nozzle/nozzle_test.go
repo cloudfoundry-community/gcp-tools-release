cbc4e2acb970a1d4870deb4c88d795aeabb3adc1
Andres Medina <amedina@pivotal.io>
2016-10-03 14:18:52 -0700

    refactored nozzle tests
    https://www.pivotaltracker.com/story/show/131454435
    
    Signed-off-by: Andres Medina <amedina@pivotal.io>
    Signed-off-by: Alpha Chen <achen@pivotal.io>

--- src/stackdriver-nozzle/nozzle/nozzle_test.go
+++ src/stackdriver-nozzle/nozzle/nozzle_test.go
@@ -12,13 +12,13 @@ import (
 var _ = Describe("Nozzle", func() {
 
 	var (
-		mockStackdriverClient *MockStackdriverClient
-		subject               nozzle.Nozzle
+		sdClient *MockStackdriverClient
+		subject  nozzle.Nozzle
 	)
 
 	BeforeEach(func() {
-		mockStackdriverClient = &MockStackdriverClient{}
-		subject = nozzle.Nozzle{StackdriverClient: mockStackdriverClient}
+		sdClient = &MockStackdriverClient{}
+		subject = nozzle.Nozzle{StackdriverClient: sdClient}
 	})
 
 	Context("logging", func() {
@@ -32,58 +32,28 @@ var _ = Describe("Nozzle", func() {
 		})
 
 		It("ships something to the stackdriver client", func() {
-			var postedEvent interface{}
-			mockStackdriverClient.PostLogFn = func(e interface{}, _ map[string]string) {
-				postedEvent = e
-			}
-
 			subject.HandleEvent(envelope)
 
-			Expect(postedEvent).To(Equal(nozzle.Envelope{envelope}))
+			postedLog := sdClient.postedLogs[0]
+			Expect(postedLog.payload).To(Equal(nozzle.Envelope{envelope}))
+			Expect(postedLog.labels).To(Equal(map[string]string{
+				"event_type": "HttpStartStop",
+			}))
 		})
 
 		It("ships multiple events", func() {
-			count := 0
-			mockStackdriverClient.PostLogFn = func(e interface{}, _ map[string]string) {
-				count += 1
-			}
-
 			for i := 0; i < 10; i++ {
 				subject.HandleEvent(envelope)
 			}
 
-			Expect(count).To(Equal(10))
-		})
-
-		It("creates labels from the event", func() {
-			var labels map[string]string
-			mockStackdriverClient.PostLogFn = func(e interface{}, sentLabels map[string]string) {
-				labels = sentLabels
-			}
-
-			subject.HandleEvent(envelope)
-
-			Expect(labels).To(Equal(map[string]string{
-				"event_type": "HttpStartStop",
-			}))
+			Expect(len(sdClient.postedLogs)).To(Equal(10))
 		})
 	})
 
 	Context("metrics", func() {
 		var envelope *events.Envelope
 
-		It("should post the metric", func() {
-			var name string
-			var value float64
-			var labels map[string]string
-
-			mockStackdriverClient.PostMetricFn = func(n string, v float64, l map[string]string) error {
-				name = n
-				value = v
-				labels = l
-				return nil
-			}
-
+		It("should post the value metric", func() {
 			metricName := "memoryStats.lastGCPauseTimeNS"
 			metricValue := float64(536182)
 			metricType := events.Envelope_ValueMetric
@@ -101,17 +71,16 @@ var _ = Describe("Nozzle", func() {
 			err := subject.HandleEvent(envelope)
 			Expect(err).To(BeNil())
 
-			Expect(name).To(Equal(metricName))
-			Expect(value).To(Equal(metricValue))
-			Expect(labels).To(Equal(map[string]string{
+			postedMetric := sdClient.postedMetrics[0]
+			Expect(postedMetric.name).To(Equal(metricName))
+			Expect(postedMetric.value).To(Equal(metricValue))
+			Expect(postedMetric.labels).To(Equal(map[string]string{
 				"event_type": "ValueMetric",
 			}))
 		})
 
 		It("returns error if client errors out", func() {
-			mockStackdriverClient.PostMetricFn = func(string, float64, map[string]string) error {
-				return errors.New("fail")
-			}
+			sdClient.postMetricError = errors.New("fail")
 
 			err := subject.HandleEvent(envelope)
 
@@ -122,19 +91,29 @@ var _ = Describe("Nozzle", func() {
 })
 
 type MockStackdriverClient struct {
-	PostLogFn    func(interface{}, map[string]string)
-	PostMetricFn func(string, float64, map[string]string) error
+	postedLogs    []PostedLog
+	postedMetrics []PostedMetric
+
+	postMetricError error
 }
 
 func (m *MockStackdriverClient) PostLog(payload interface{}, labels map[string]string) {
-	if m.PostLogFn != nil {
-		m.PostLogFn(payload, labels)
-	}
+	m.postedLogs = append(m.postedLogs, PostedLog{payload, labels})
 }
 
 func (m *MockStackdriverClient) PostMetric(name string, value float64, labels map[string]string) error {
-	if m.PostMetricFn != nil {
-		return m.PostMetricFn(name, value, labels)
-	}
-	return nil
+	m.postedMetrics = append(m.postedMetrics, PostedMetric{name, value, labels})
+
+	return m.postMetricError
+}
+
+type PostedLog struct {
+	payload interface{}
+	labels  map[string]string
+}
+
+type PostedMetric struct {
+	name   string
+	value  float64
+	labels map[string]string
 }
