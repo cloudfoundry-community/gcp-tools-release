7b230864e2100ef5328e578bd3ccb2ca4b47ee24
Jeff Johnson <jrjohnson@google.com>
2017-01-30 13:12:35 -0800

    script doesn't exist in cf tile image (and isn't needed)

--- ci/tasks/build-tile.sh
+++ ci/tasks/build-tile.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
-
 set -e
-
 source stackdriver-tools/ci/tasks/utils.sh
-source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 
-cpi_release_name="stackdriver-tools"
+release_name="stackdriver-tools"
 semver=`cat version-semver/number`
 
-image_name=${cpi_release_name}-${semver}.tgz
+image_name=${release_name}-${semver}.tgz
 image_path="https://storage.googleapis.com/bosh-gcp/beta/stackdriver-tools/${image_name}"
 output_path=candidate/latest-nozzle-tile.pivotal
 
