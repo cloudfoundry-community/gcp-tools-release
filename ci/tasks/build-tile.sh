52c3f9cf7f734aebd1a413689ffb016753d31157
Jeff Johnson <jrjohnson@google.com>
2017-01-30 13:06:39 -0800

    use the tile generator docker image

--- ci/tasks/build-tile.sh
+++ ci/tasks/build-tile.sh
@@ -12,5 +12,12 @@ image_name=${cpi_release_name}-${semver}.tgz
 image_path="https://storage.googleapis.com/bosh-gcp/beta/stackdriver-tools/${image_name}"
 output_path=candidate/latest-nozzle-tile.pivotal
 
+pushd "stackdriver-tools"
+	echo "building tile"
+	tile build ${semver}
+popd
+
 echo "${image_path}"
-echo "${image_path}" > ${output_path} 
+
+echo "exposing tile"
+mv stackdriver-tools/product/*.pivotal ${output_path}
